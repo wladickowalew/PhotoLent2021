db3c849be06889fff0ec4e50fd7aa34d15fdfb8e
Wlad Kowalew <wladickowalew@gmail.com>
2021-03-28 13:37:34 +0300

    client begin. add Register component

---
+++ client/components/register.vue
@@ -0,0 +1,34 @@
+<style scoped>
+	#reg{
+		background: red;
+		width: 100%;
+		border-color: grey;
+	}
+	input, #reg{
+		box-shadow: 0 0 10px rgba(0,0,0,0.5);
+	}
+	form{
+		width: 700px;
+	}
+	*{
+		font-size: 25px;
+	}
+</style>
+
+<template>
+  <form>
+	  <div class="mb-3">
+	    <label for="login" class="form-label">Логин</label>
+	    <input type="text" class="form-control" id="login">
+	  </div>
+	  <div class="mb-3">
+	    <label for="password1" class="form-label">Пароль</label>
+	    <input type="password" class="form-control" id="password1">
+	  </div>
+	  <div class="mb-3">
+	    <label for="password2" class="form-label">Повторите пароль</label>
+	    <input type="password" class="form-control" id="password2">
+	  </div>
+	  <button type="submit" class="btn btn-primary" id="reg">Зарегистрироваться</button>
+  </form>
+</template>
---
+++ client/script.js
@@ -0,0 +1,25 @@
+let Vue         = require("vue");
+let VueRouter   = require("vue-router");
+let VueResource = require("vue-resource");
+let vjm         = require("vue-jwt-mongo");
+
+let app      = require("./components/app.vue");
+let register = require("./components/register.vue");
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+Vue.use(vjm.Client);
+
+let router = new VueRouter({
+	routes:[
+		{path:"/register", component: register}
+	]
+});
+
+let vm = new Vue({
+	el: "#app",
+	router: router,
+	render: function (createElements) {
+		return createElements(app);
+	}
+});
