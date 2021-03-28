34b008063e2d6212cc69dfc79f6c3d4c898b0e3e
Wlad Kowalew <wladickowalew@gmail.com>
2021-03-28 13:47:51 +0300

    add login component

---
+++ client/components/login.vue
@@ -0,0 +1,38 @@
+<style scoped>
+	#log{
+		background: blue;
+		width: 100%;
+		border-color: grey;
+		margin-top: 20px;
+	}
+	input, #log{
+		box-shadow: 0 0 10px rgba(0,0,0,0.5);
+	}
+	form{
+		width: 700px;
+	}
+	*{
+		font-size: 25px;
+		margin-top: 10px;
+	}
+	a{
+		font-size: 17px;
+	}
+</style>
+
+<template>
+	<div>
+		<form id="login_form">
+		  <div class="form-group">
+		    <label for="login">Логин</label>
+		    <input type="text" class="form-control" id="login" placeholder="Введите Логин" name="login">
+		  </div>
+		  <div class="form-group">
+		    <label for="password">Пароль</label>
+		    <input type="password" class="form-control" id="password" placeholder="Введите пароль" name="password">
+		  </div>
+		  <button type="submit" class="btn btn-primary" id="log">Войти!</button>
+		  <center><a href="http://localhost:8080/#/register" class="form-link" >Регистрация</a></center>
+		</form>
+	</div>
+</template>
--- client/script.js
+++ client/script.js
@@ -5,6 +5,7 @@ let vjm         = require("vue-jwt-mongo");
 
 let app      = require("./components/app.vue");
 let register = require("./components/register.vue");
+let login    = require("./components/login.vue");
 
 Vue.use(VueRouter);
 Vue.use(VueResource);
@@ -12,7 +13,8 @@ Vue.use(vjm.Client);
 
 let router = new VueRouter({
 	routes:[
-		{path:"/register", component: register}
+		{path:"/register", component: register},
+		{path:"/login",    component: login}
 	]
 });
 
